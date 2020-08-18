0156c8449bc6d2c8b8f99929e54a146c9419394e
kohey127 <koooochoki@gmail.com>
2020-08-18 16:30:01 +0900

    [debug-2]Start

--- bookers1_debug/app/controllers/books_controller.rb
+++ bookers1_debug/app/controllers/books_controller.rb
@@ -23,7 +23,6 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "successfully updated book!"
     else
